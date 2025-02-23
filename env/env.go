120fddad3f63aad8bf3fd0902606313a9ae52d46
Anshydv18 <anshgithub@gmail.com>
2025-02-23 19:06:01 +0530

    env

--- env/env.go
+++ env/env.go
@@ -3,14 +3,22 @@ package env
 import (
 	"log"
 	"os"
+	"sync"
 
 	"github.com/joho/godotenv"
 )
 
+var once sync.Once
+
 func LoadEnv() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	once.Do(func() {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("⚠️ Warning: .env file not found")
+		} else {
+			log.Println("✅ .env file loaded successfully.")
+		}
+	})
 }
 
 func Get(key string) string {
